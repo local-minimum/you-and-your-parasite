b4c34aadb81b2863a02495d69f75c49728db8d92
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-12-12 18:37:13 +0100

    Interaction is begun

--- Assets/Scripts/UI/DialogMonitor.cs
+++ Assets/Scripts/UI/DialogMonitor.cs
@@ -3,25 +3,25 @@ using System.Collections;
 using UnityEngine.UI;
 
 
-public enum DialogOutcome { Grow, Shrink};
-
-public delegate void CompletedInteraction(DialogOutcome outcome);
-
 public class DialogMonitor : MonoBehaviour {
 
-    public static event CompletedInteraction OnCompletedInteraction;
 
     [SerializeField]
     DialogMeter meter;
 
+    void OnEnable()
+    {
+        DialogSystem.OnNewAnswer += Response;
+    }
+
+    void OnDisable()
+    {
+        DialogSystem.OnNewAnswer -= Response;
+    }
+
 	void Response(DialogOutcome response)
     {
         meter.Increase(response);
-        if (meter.Completed)
-        {
-            if (OnCompletedInteraction != null)
-                OnCompletedInteraction(meter.GrowthOutcome ? DialogOutcome.Grow : DialogOutcome.Shrink);
-            meter.Reset();
-        }
+
     }
 }
--- Assets/Scripts/UI/DialogSystem.cs
+++ Assets/Scripts/UI/DialogSystem.cs
@@ -19,11 +19,18 @@ public class DialogState
 
 public enum AnswerSide { Left, Right};
 
+public enum DialogCycle {None, Intro, PlayerInput, Answer, Reaction};
+
 public delegate void AnswerType(DialogOutcome type);
+
+public enum DialogOutcome { Grow, Shrink };
+
+public delegate void CompletedInteraction(DialogOutcome outcome);
 
 public class DialogSystem : MonoBehaviour {
 
     public static event AnswerType OnNewAnswer;
+    public static event CompletedInteraction OnCompletedDialog;
 
     [SerializeField]
     Text mainText;
@@ -43,24 +50,49 @@ public class DialogSystem : MonoBehaviour {
     [SerializeField]
     DialogState[] questions;
 
-    int questionIndex = 0;    
+    int questionIndex = 0;
+
+    KeyCode leftKey;
+    KeyCode rightKey;
+
+    bool talking;
+
+    DialogCycle cycleStep;
+    DialogOutcome answerOutcome;
+
+    int growths = 0;
 
     Dictionary<AnswerSide, DialogOutcome> answers = new Dictionary<AnswerSide, DialogOutcome>();
     
 	void Start () {
+        leftKey = (KeyCode)PlayerPrefs.GetInt("Key.X", (int)'x');
+        rightKey = (KeyCode)PlayerPrefs.GetInt("Key.C", (int)'c');
         mainText.enabled = false;
         inAnswerMode = false;
 	}
+
+    public void AnswerLeft()
+    {
+        Answer(AnswerSide.Left);
+    }
+
+    public void AnswerRight()
+    {
+        Answer(AnswerSide.Right);
+    }
 
     public void Answer(AnswerSide answer)
     {
         if (OnNewAnswer != null)
         {
-            OnNewAnswer(answers[answer]);
+            OnNewAnswer(answers[ answer]);
         }
-        mainText.text = answers[answer] == DialogOutcome.Grow ? questions[questionIndex].ReactionGrow : questions[questionIndex].ReactionShrink;
-        questionIndex++;
-        inAnswerMode = false;
+        answerOutcome = answers[answer];
+
+        if (answerOutcome == DialogOutcome.Grow)
+            growths++;
+
+        StepCycle();
     }
 
     bool inAnswerMode {
@@ -70,6 +102,7 @@ public class DialogSystem : MonoBehaviour {
             buttonRight.SetActive(value);
             answerLeft.enabled = value;
             answerRight.enabled = value;
+            mainText.enabled = !value;
         }
     }
 
@@ -81,8 +114,80 @@ public class DialogSystem : MonoBehaviour {
 
         answerLeft.text = reversed ? questions[questionIndex].HintGrow : questions[questionIndex].HintShrink;
         answerRight.text = reversed ? questions[questionIndex].HintShrink : questions[questionIndex].HintGrow;
+    }
 
-        inAnswerMode = true;
+    void Update()
+    {
+        if (talking)
+        {
+            if (Input.GetKeyDown(leftKey) || (Input.GetKeyDown(rightKey)))
+                StepCycle();
+        }
+        else if (cycleStep == DialogCycle.PlayerInput)
+        {
+            if (Input.GetKeyDown(leftKey))
+                AnswerLeft();
+            else if (Input.GetKeyDown(rightKey))
+                AnswerRight();
+        }
+        else if (cycleStep == DialogCycle.None)
+            StepCycle();
+
+    }
+
+    void StepCycle()
+    {
+        Debug.Log(cycleStep);
+        if (cycleStep == DialogCycle.Intro)
+        {         
+            SetAnswers();
+            inAnswerMode = true;
+            cycleStep = DialogCycle.PlayerInput;
+
+        } else if (cycleStep == DialogCycle.PlayerInput)
+        {
+            mainText.text = answerOutcome == DialogOutcome.Grow ? questions[questionIndex].AnswerGrow : questions[questionIndex].AnswerShrink;
+            inAnswerMode = false;
+            cycleStep = DialogCycle.Answer;
+        } else if (cycleStep == DialogCycle.Answer)
+        {
+            if (questionIndex == questions.Length - 1)
+            {
+                Complete();
+            } else
+            {
+                mainText.text = answerOutcome == DialogOutcome.Grow ? questions[questionIndex].ReactionGrow : questions[questionIndex].ReactionShrink;
+            }
+
+            cycleStep = DialogCycle.Reaction;
+        } else if (cycleStep == DialogCycle.Reaction || cycleStep == DialogCycle.None)
+        {
+            if (cycleStep == DialogCycle.None)
+                questionIndex = 0;
+            else
+                questionIndex++;
+
+            if (questionIndex < questions.Length)
+            {
+                cycleStep = DialogCycle.Intro;
+                mainText.text = questions[questionIndex].Intro;
+            }
+        }
+
+        talking = cycleStep != DialogCycle.PlayerInput;
+
+    }
+
+    void Complete()
+    {
+        talking = false;
+        cycleStep = DialogCycle.None;
+        if (OnCompletedDialog != null)
+        {
+            OnCompletedDialog(growths > questions.Length / 2 ? DialogOutcome.Grow : DialogOutcome.Shrink);
+        }
+        growths = 0;
+        Application.UnloadLevel("/interaction");
     }
 
 }
