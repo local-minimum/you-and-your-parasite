23d081859c18fc07346af6c648ab4686acc0dbdf
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-12-13 19:59:40 +0100

    Crystals

---
+++ Assets/Scripts/RockCrystal.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockCrystal : MonoBehaviour {
+
+    GameObject seed;
+    GameObject[] others;
+
+    float nextSpawn;
+
+    [SerializeField, Range(0, 10)]
+    float minTime = 1;
+
+    [SerializeField, Range(0, 10)]
+    float maxTime = 3;
+
+    int visibleIndex = -1;
+
+	// Use this for initialization
+	void Start () {
+        seed = transform.GetChild(0).gameObject;
+        others = new GameObject[transform.childCount - 1];
+        for (int i = 0; i < others.Length; i++)
+            others[i] = transform.GetChild(i + 1).gameObject;
+	}
+
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad > nextSpawn)
+        {
+            visibleIndex++;
+            if (visibleIndex > others.Length)
+                visibleIndex = 0;
+            SetVisible();
+            nextSpawn = Time.timeSinceLevelLoad + Random.Range(minTime, maxTime);
+
+        }
+    }
+
+    void SetVisible() {
+        for (int i = 0; i < others.Length; i++)
+            others[i].SetActive(i < visibleIndex);
+    }	
+}
