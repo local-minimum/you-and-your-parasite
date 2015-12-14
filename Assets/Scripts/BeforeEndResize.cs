47d8650d77bd955dc70eede90b601d23f20ac216
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-12-14 17:26:37 +0100

    Music

---
+++ Assets/Scripts/BeforeEndResize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeforeEndResize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var dFilm = GetComponent<DialogFilm>();
+        if (GameMonitor.DidGrow)
+            dFilm.PlayGrow();
+        else
+            dFilm.PlayShrink();
+	}
+
+    void OnDisable()
+    {
+        GameMonitor.ResizeDone();
+    }
+}
---
+++ Assets/Scripts/Triggers/MusicSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSwitcher : MonoBehaviour {
+
+    [SerializeField]
+    AudioClip clipA;
+
+    [SerializeField]
+    AudioClip clipB;
+
+    void OnTriggerEnter(Collider other)
+    {
+        var audioSource = other.GetComponentInChildren<AudioSource>();
+        if (audioSource)
+        {
+            audioSource.Pause();
+            audioSource.clip = audioSource.clip == clipA ? clipB : clipA;
+            audioSource.Play();
+        }
+    }
+}
