911fab9956583d2e86792f23515cc808d7a21655
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-12-14 18:41:28 +0100

    Video aspect ratios

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    [SerializeField]
+    string gameScene;
+
+	public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void BrowseTo(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+}
