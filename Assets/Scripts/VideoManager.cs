8483bd856ec0481e87f3b8c60ced5c31191f3d39
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-12-15 00:27:25 +0100

    Making structure

---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class VidScene
+{
+    public Videos type;
+    public GameObject canvas;
+    public GameObject film;
+
+}
+
+public enum Videos { Intro, Talk, Resize, Amobea, Chippy, SilkWorm, Plastic, SantaGod, Toad, BeforeEndResize, EndSmall, EndBoring, EndBig};
+
+public class VideoManager : MonoBehaviour {
+
+    [SerializeField]
+    VidScene[] videos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
